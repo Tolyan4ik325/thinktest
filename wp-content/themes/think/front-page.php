f16260d4398eb96f3f3bbb83afe369d472936f07
Tolyan4ik325 <a.s.ovrutskyi@gmail.loc>
2021-07-14 14:19:35 +0300

    create a recent projects section

--- wp-content/themes/think/front-page.php
+++ wp-content/themes/think/front-page.php
@@ -23,10 +23,10 @@
 
 <!--Header item-->
 <div id="header">
-    <div class="container-fluid">
+    <div class="container">
         <div class="row header">
             <div class="logo col-md-6">
-                <img src="" alt="">
+                <img src="wp-content/themes/think/images/think-logo-white.svg" alt="">
             </div>
             <div class="menu col-md-6">
                 <img src="" alt="">
@@ -56,8 +56,133 @@
         </div>
     </div>
 </div>
+<!--About us-->
+<div id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="white">A digital product agency</h2>
+                <p class="desc white mt-4 mb-5">
+                    Combining a strategic approach, strong technical focus and a keen sense of design; we pour   passion into every project we undertake, be it web design or mobile app development, and turn your every digital project into a competitive advantage.
+                </p>
+                <a href="#" class="button-1">About Us</a>
+            </div>
+        </div>
+        <div class="row pt-4 services">
+            <div class="col-md-4">
+                <div class="item" style="background: url('/wp-content/themes/think/images/services/dev-a-website.png');">
+                    <div class="info">
+                        <h3 class="white">
+                            Develop Mobile App
+                        </h3>
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item" style="background: url('/wp-content/themes/think/images/services/dev-a-website.png');">
+                    <div class="info">
+                        <h3 class="white">
+                            Develop Mobile App
+                        </h3>
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item" style="background: url('/wp-content/themes/think/images/services/dev-mobile-app.png');">
+                    <div class="info">
+                        <h3 class="white">
+                            Develop Mobile App
+                        </h3>
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="item" style="background: url('/wp-content/themes/think/images/services/dev-a-website.png');">
+                    <div class="info">
+                        <h3 class="white">
+                            Develop Mobile App
+                        </h3>
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item" style="background: url('/wp-content/themes/think/images/services/dev-a-website.png');">
+                    <div class="info">
+                        <h3 class="white">
+                            Develop Mobile App
+                        </h3>
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item" style="background: url('/wp-content/themes/think/images/services/dev-mobile-app.png');">
+                    <div class="info">
+                        <h3 class="white">
+                            Develop Mobile App
+                        </h3>
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="item" style="background: url('/wp-content/themes/think/images/services/dev-a-website.png');">
+                    <div class="info">
+                        <h3 class="white">
+                            Develop Mobile App
+                        </h3>
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="item" style="background: url('/wp-content/themes/think/images/services/our-work.png');">
+                    <div class="info">
+                        <h3 class="white">
+                            Develop Mobile App
+                        </h3>
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Recent Projects item-->
+<div id="recent-projects">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="dark">Take a look at our most
+                    recent projects</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="item">
+                    <img src="wp-content/themes/think/images/lava-rewards-project/01.png" alt="">
+                    <h3 class="dark">Lava Rewards</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                    <ul>
+                        <li><a href="#">Web</a></li>
+                        <li><a href="#">Mobile</a></li>
+                        <li><a href="#">Branding</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+</div>
 <body>
-<h1>Test</h1>
 
 
 
